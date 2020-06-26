34935bb73553cb29ad40c482758c26f3eb87444e
Sarah Peters <sarahpieters15@gmail.com>
2020-06-26 13:53:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,7 @@ class Shoe
 
   def initialize(brand)
       @brand = brand
-      if BRANDS.include?(brand)
-      else
-        BRANDS << brand
-      end
+      BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
